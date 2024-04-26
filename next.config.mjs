54a9afbe441581f8249cdeb6ca84023c6085f3ea
Marcelo Macedo <marcelo3macedo@gmail.com>
2024-04-26 10:24:34 -0300

    fix: next-config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 import createNextIntlPlugin from "next-intl/plugin";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 const withNextIntl = createNextIntlPlugin();
 
-export default withNextIntl(nextConfig);
+export default withNextIntl({});
