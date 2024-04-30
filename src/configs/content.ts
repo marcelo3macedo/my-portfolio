d5266d4f8a88970220b56bebedbb37acb39fd8a0
Marcelo Macedo <marcelo3macedo@gmail.com>
2024-04-30 19:44:55 -0300

    feat: new projects

--- src/configs/content.ts
+++ src/configs/content.ts
@@ -81,6 +81,48 @@ const projects = [
       },
     ],
   },
+  {
+    title: "list.memo.title",
+    contents: [
+      { type: "text", value: "list.memo.intro" },
+      { type: "text", value: "list.memo.details" },
+      { type: "text", value: "list.memo.types" },
+      { type: "text", value: "list.memo.link" },
+      {
+        type: "link",
+        href: "https://play.google.com/store/apps/details?id=com.app.memorizou&hl=en_US",
+        value: "action.access",
+      },
+    ],
+  },
+  {
+    title: "list.abcshow.title",
+    contents: [
+      { type: "text", value: "list.abcshow.intro" },
+      { type: "text", value: "list.abcshow.details" },
+      { type: "text", value: "list.abcshow.types" },
+      { type: "text", value: "list.abcshow.link" },
+      {
+        type: "link",
+        href: "https://play.google.com/store/apps/details?id=com.app.abcshow&hl=ar",
+        value: "action.access",
+      },
+    ],
+  },
+  {
+    title: "list.customup.title",
+    contents: [
+      { type: "text", value: "list.customup.intro" },
+      { type: "text", value: "list.customup.details" },
+      { type: "text", value: "list.customup.types" },
+      { type: "text", value: "list.customup.link" },
+      {
+        type: "link",
+        href: "https://www.youtube.com/watch?v=E9Pt-P63vKE",
+        value: "action.access",
+      },
+    ],
+  },
 ];
 
 export { highlights, education, projects };
